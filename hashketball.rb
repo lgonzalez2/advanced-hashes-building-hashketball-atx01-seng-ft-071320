9fe95f0af6af1afd0955bf92ebd88156c2f58aaf
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 19:06:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,7 +32,15 @@ def game_hash
           :steals,
           :blocks,
           :slam_dunks},
-        {},
+        {:player_name,
+          :number,
+          :shoe,
+          :points,
+          :rebounds,
+          :assists,
+          :steals,
+          :blocks,
+          :slam_dunks},
         {}]
     },
     :away => {
