fda3a55805f96f131cb7d99a228c606d72b8a9f0
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 19:01:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,14 @@
 def game_hash 
   nested_hash = {
     :home => {
-      :team_name => string,
+      :team_name => "Brooklyn Nets",
       :colors => [],
       :players => []
     },
     :away => {
-      
+      :team_name => "Charlotte Hornets",
+      :colors => [],
+      :players => []
     }
   }
   
