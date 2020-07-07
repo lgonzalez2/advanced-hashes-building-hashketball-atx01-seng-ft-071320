e9dde7090eaac93ae1033d00ea1795bbe26dda54
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 19:07:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -41,12 +41,25 @@ def game_hash
           :steals,
           :blocks,
           :slam_dunks},
-        {}]
+        {:player_name,
+          :number,
+          :shoe,
+          :points,
+          :rebounds,
+          :assists,
+          :steals,
+          :blocks,
+          :slam_dunks}]
     },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => []
+      :players => [
+        {},
+        {},
+        {},
+        {},
+        {}]
     }
   }
   
